fb9142f23dcfc50400b8e54d706a44094db15a45
Elton Mário da Silva <eltonjf@gmail.com>
2016-04-02 20:32:36 -0300

    Final commit. The app is done.

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,8 +2,62 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
+
+    public void addOneForTeamA(View v){
+        scoreTeamA += 1;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void addTwoForTeamA(View v){
+        scoreTeamA += 2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void addThreeForTeamA(View v){
+        scoreTeamA += 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void displayForTeamA(int scoreTeamA) {
+        TextView text_view_score_tema_a = (TextView) findViewById(R.id.text_view_score_team_a);
+        text_view_score_tema_a.setText(String.valueOf(scoreTeamA));
+    }
+
+
+    public void addOneForTeamB(View v){
+        scoreTeamB += 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addTwoForTeamB(View v){
+        scoreTeamB += 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addThreeForTeamB(View v){
+        scoreTeamB += 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void displayForTeamB(int scoreTeamB) {
+        TextView text_view_score_tema_b = (TextView) findViewById(R.id.text_view_score_team_b);
+        text_view_score_tema_b.setText(String.valueOf(scoreTeamB));
+    }
+
+
+    public void resetScore(View v){
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
